0c2d10105e0578b80f51e7bdeafa7bed8929e795
skyorion427 <skyorion427@gmail.com>
2021-03-03 01:56:38 -0500

    Updated: example usage

--- examples/sdk.ts
+++ examples/sdk.ts
@@ -5,12 +5,15 @@ const thorchain = new ThorchainSDK({ network: 'testnet', phrase: 'xxxx' })
 const useThorchain = async () => {
   try {
     // quote swap
-    const swapEntity = await thorchain.quote('BTC.BTC', 'BNB.BNB', 100)
+    const swapEntity = thorchain.quote('BTC.BTC', 'BNB.BNB', 100)
 
     console.log(swapEntity.outputAmount.assetAmount.toNumber())
     console.log(swapEntity.slip.assetAmount.toNumber())
     console.log(swapEntity.hasInSufficientFee)
     console.log(swapEntity.estimatedNetworkFee.assetAmount.toNumber())
+
+    // refresh
+    thorchain.refresh()
 
     // execute swap
     const txExplorerUrl = await thorchain.swap(swapEntity)
